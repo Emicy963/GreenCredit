e9daab4f75b9b042b8f5f5d7238caa58e33c757f
Anderson Cafurica <129119434+Emicy963@users.noreply.github.com>
2025-02-19 01:43:20 +0100

    Creat a new URLS and Root for Statics Files and Media Files

--- setup/urls.py
+++ setup/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
 from accounts.views import auth_view, logout_view
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -8,3 +10,7 @@ urlpatterns = [
     path('auth/', auth_view, name='auth'),
     path('logout/', logout_view, name='logout')
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
