a77f40b073bf8f826ab36d2014f25c3208072400
Anderson Cafurica <129119434+Emicy963@users.noreply.github.com>
2025-02-19 02:34:12 +0100

    Working in template and view for List the createad product

--- greencredit/views.py
+++ greencredit/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView, DetailView
+from .models import Produto
+from .forms import ProdutoForm
 
 def home(request):
     return render(request, 'home.html')
@@ -9,3 +11,15 @@ def about(request):
 
 def contact(request):
     return render(request, 'contact.html')
+
+class ProdutoListView(ListView):
+    model = Produto
+    template_name = 'produto_list.html'
+    context_object_name = 'produtos'
+    paginate_by = 10
+
+class ProdutoCreateView(CreateView):
+    model = Produto
+    form_class = ProdutoForm
+    template_name = 'produto_form.html'
+    success_url = '/produtos/'
