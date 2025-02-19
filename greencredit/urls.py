3be9434738c4627e5a6e0d3665fb29e0e60d6cf5
Anderson Cafurica <129119434+Emicy963@users.noreply.github.com>
2025-02-19 02:43:34 +0100

    Create a urls for Update and Delete produtos

--- greencredit/urls.py
+++ greencredit/urls.py
@@ -2,7 +2,13 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('home/', views.home, name='home'),
+    path('', views.home, name='home'),
     path('about/', views.about, name='about'),
     path('contact/', views.contact, name='contact'),
+    # Produtos
+    path('produtos/', views.ProdutoListView.as_view(), name='produto_list'),
+    path('produtos/novo/', views.ProdutoCreateView.as_view(), name='produto_create'),
+    path('produtos/editar/<int:id>/', views.ProdutoUpdateView.as_view(), name='produto_update'),
+    path('produtos/exluir/<int:id>/', views.ProdutoDeleteView.as_view(), name='produto_delete'),
+
 ]
